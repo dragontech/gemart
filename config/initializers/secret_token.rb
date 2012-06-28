0433bef97652a8b812b632f77d7b9950916f6d3a
Mihai Frisan <mihai@frisan.ro>
2012-06-28 17:33:30 +0300

    set default spree store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GemartRo::Application.config.secret_token = '9d95f1a44f93c9cf0543921289d9a9a8cf55070e945356231e04c3dca4f80e2c43237b22a34818b80cda2e741a0db431ee39499bc892be7b1aa73fc294fa23c3'
